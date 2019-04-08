d122e3526e4f976d3e2c0474da480acba6c15cc3
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-04-08 18:55:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,7 +28,7 @@ class Application
       if @@items.include? new_item
         @@cart << new_item
         resp.write "added #{new_item}\n"
-      else resp.write ""
+      else resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
